f5d87e3013f1bb5d517c7ef4737706e0b582445f
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-05-24 11:35:25 -0700

    server: Skeleton server

--- src/server.zig
+++ src/server.zig
@@ -6,18 +6,40 @@ pub fn main() !void {
     defer network.deinit();
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer if (gpa.deinit() == .leak) @panic("Memory leak detected!");
+    defer if (gpa.deinit() == .leak) @panic("Memory Leak!");
+
     const allocator = gpa.allocator();
-    _ = allocator;
+
+    const port_number = 7777;
 
     var socket = try network.Socket.create(.ipv4, .tcp);
     defer socket.close();
 
-    //Bind to port 70 (gopher)
-    // try socket.bindToPort(70);
-    try socket.bindToPort(7777);
+    try socket.bindToPort(port_number);
 
     try socket.listen();
 
-    while (true) {}
+    var buf = try allocator.alloc(u8, 1024);
+    defer allocator.free(buf);
+    while (true) {
+        var client = try socket.accept();
+        defer client.close();
+
+        std.debug.print("Client connected from {}.\n", .{
+            try client.getLocalEndPoint(),
+        });
+
+        var reader = client.reader();
+        var writer = client.writer();
+
+        var selector = (try reader.readUntilDelimiterOrEof(buf, '\n') orelse break);
+
+        //Strip the \r at the end
+        selector = selector[0 .. selector.len - 1];
+
+        std.debug.print("Got selector \"{s}\"\n", .{selector});
+
+        try writer.writeAll("iThis is a test!\t\terror.host\t1\r\n");
+        try writer.writeAll(".\r\n");
+    }
 }
