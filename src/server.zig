d4be35323d39e34cbcda802dfbee766f010e42de
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-05-24 11:49:48 -0700

    server: Handle connections on another thread

--- src/server.zig
+++ src/server.zig
@@ -46,27 +46,41 @@ pub fn main() !void {
 
     std.debug.print("Listening...\n", .{});
 
-    var buf = try allocator.alloc(u8, 1024);
-    defer allocator.free(buf);
     while (true) {
         var client = try socket.accept();
-        defer client.close();
 
-        std.debug.print("Client connected from {}.\n", .{
-            try client.getLocalEndPoint(),
-        });
-
-        var reader = client.reader();
-        var writer = client.writer();
-
-        var selector = (try reader.readUntilDelimiterOrEof(buf, '\n') orelse break);
-
-        //Strip the \r at the end
-        selector = selector[0 .. selector.len - 1];
-
-        std.debug.print("Got selector \"{s}\"\n", .{selector});
-
-        try writer.writeAll("iThis is a test!\t\terror.host\t1\r\n");
-        try writer.writeAll(".\r\n");
+        //Spawn a new thread to handle the client
+        var t = try std.Thread.spawn(.{}, runClient, .{ client, allocator });
+        //Detatch the thread, let it LIVE
+        t.detach();
     }
+}
+
+fn runClient(client: network.Socket, allocator: std.mem.Allocator) !void {
+    defer client.close();
+
+    //Create a new buffer to store our working data
+    var buf = try allocator.alloc(u8, 1024);
+    defer allocator.free(buf);
+
+    //Print that we have got a new client
+    std.debug.print("Client connected from {}.\n", .{
+        try client.getLocalEndPoint(),
+    });
+
+    var reader = client.reader();
+    var writer = client.writer();
+
+    //Read until the next \n, if EOF, return out
+    var selector = (try reader.readUntilDelimiterOrEof(buf, '\n') orelse return);
+
+    //Strip the \r at the end
+    selector = selector[0 .. selector.len - 1];
+
+    //Print the selector we recieved
+    std.debug.print("Got selector \"{s}\"\n", .{selector});
+
+    try writer.writeAll("iThis is a test!\t\terror.host\t1\r\n");
+    //Write the end of the connection
+    try writer.writeAll(".\r\n");
 }
